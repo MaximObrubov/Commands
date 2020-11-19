0b08257a195bac94e01e08d786b0c9ed031b86ea
Maxim Obrubov <m.obrubov@yandex.ru>
2020-11-19 17:38:56 +0400

    added some further crawelers

--- crawls.sh
+++ crawls.sh
@@ -8,7 +8,11 @@ show_menu(){
     printf "\n${menu}*********************************************${normal}\n"
     printf "${menu}**${number} 1)${menu} Googlebot ${normal}\n"
     printf "${menu}**${number} 2)${menu} Facebookbot ${normal}\n"
-    printf "${menu}**${number} 3)${menu} User browser ${normal}\n"
+    printf "${menu}**${number} 3)${menu} VKontakte ${normal}\n"
+    printf "${menu}**${number} 4)${menu} Odnoklassniki ${normal}\n"
+    printf "${menu}**${number} 5)${menu} LinkedInBot ${normal}\n"
+    printf "${menu}**${number} 6)${menu} MailRuBot ${normal}\n"
+    printf "${menu}**${number} 7)${menu} User browser ${normal}\n"
     printf "${menu}**${number} x)${menu} ${fgred}exit ${normal}\n"
     printf "${menu}*********************************************${normal}\n"
     printf "Request as a: ${normal}"
@@ -41,6 +45,26 @@ while [ $opt != '' ]
             show_menu;
         ;;
         3) clear;
+            printf "CURL as VKontakte:\n\n"
+            curl -H "User-Agent: Mozilla/5.0 (compatible; vkShare; +http://vk.com/dev/Share)" "$1"
+            show_menu;
+        ;;
+        4) clear;
+            printf "CURL as user:\n\n"
+            curl -H "User-Agent: Mozilla/5.0 (compatible; OdklBot/1.0 like Linux; klass@odnoklassniki.ru)" "$1"
+            show_menu;
+        ;;
+        5) clear;
+            printf "CURL as user:\n\n"
+            curl -H "User-Agent: LinkedInBot/1.0 (compatible; Mozilla/5.0; Apache-HttpClient http://www.linkedin.com)" "$1"
+            show_menu;
+        ;;
+        6) clear;
+            printf "CURL as user:\n\n"
+            curl -H "User-Agent: Mozilla/5.0 (compatible; Linux x86_64; Mail.RU_Bot/Fast/2.0; +http://go.mail.ru/help/robots) python-requests/2.2.1 CPython/2.7.6 Linux/4.9.80-c9" "$1"
+            show_menu;
+        ;;
+        7) clear;
             printf "CURL as user:\n\n"
             curl -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.89 Safari/537.36" "$1"
             show_menu;
